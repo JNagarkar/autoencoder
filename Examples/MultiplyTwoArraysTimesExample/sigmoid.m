828c6ac68be93e82cc498e8f4fd0d1dc8353d49a
JNagarkar <jaydatta.nagarkar@asu.edu>
2016-12-06 14:54:41 -0700

    Removed unwanted files

--- Examples/MultiplyTwoArraysTimesExample/sigmoid.m
+++
@@ -1,8 +0,0 @@
-function [ neuralNet] = sigmoid(neuralNet)
-%UNTITLED9 Summary of this function goes here
-%   Detailed explanation goes here
-
-neuralNet = 1.0 ./ ( 1.0 + exp(-neuralNet) );
-
-end
-
